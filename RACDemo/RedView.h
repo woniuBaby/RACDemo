0be50d5ec3275f58c143e78dcfe6f5e72fb92565
yanjinchao <yanjinchao@54.com>
2019-10-29 10:59:06 +0800

    first commit

---
+++ RACDemo/RedView.h
@@ -0,0 +1,17 @@
+//
+//  RedView.h
+//  RACDemo
+//
+//  Created by yanjinchao on 2019/10/16.
+//  Copyright © 2019 yanjinchao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RedView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ RACDemo/RedView.m
@@ -0,0 +1,21 @@
+//
+//  RedView.m
+//  RACDemo
+//
+//  Created by yanjinchao on 2019/10/16.
+//  Copyright © 2019 yanjinchao. All rights reserved.
+//
+
+#import "RedView.h"
+
+@implementation RedView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
